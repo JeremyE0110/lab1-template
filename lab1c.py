0c2ac57e2f682923add5c505da82cfe52865b3fd
Jeremy Evangelista <jevangelista5@myseneca.ca>
2023-05-22 13:46:27 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#num1 = 5
+#num2 = 10
+
+#print(num1)
+#print(num2)
+
+#sum = num1 + num2
+
+#print(sum)
+
+# #Error not int
+#print('The sum is: ' + sum) 
+
+
+#print('The sum is: ' + str(sum))
+
+#Practice
+name = 'Isaac' 
+age = 72
+
+print(name + ' is ' + str(age) +' years old!')
