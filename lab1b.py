704fc61a2c06b8e40c7bba7f5e4abe55b137ad22
Jeremy Evangelista <jevangelista5@myseneca.ca>
2023-05-22 13:31:24 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Description: This program is exploring Python's built-in Data Objects'''
+
+#Objects
+#name = 'Thomas'
+name = 'Isaac'
+
+#Will print the value of the object
+#print(name) 
+
+#Will print the 'name' instead of the object
+#print('name')  
+
+#Combination of string and object
+#print('I have a friend named ' + name)
+print('How old are you ' + name +'?')
